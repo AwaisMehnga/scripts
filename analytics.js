5c04f631d8234ab03f04af1d0c5373271829e57f
Muhammad Awais <60058402+AwaisMehnga@users.noreply.github.com>
2024-07-09 12:55:21 +0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -8,18 +8,17 @@ function getYoutubeVideoId(url) {
         return null;
     }
 
-    var videoIdRegExp = /^.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]{11}).*/;
+    var videoIdRegExp =
+        /^.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]{11}).*/;
     var videoIdMatch = url.match(videoIdRegExp);
 
-    return (videoIdMatch && videoIdMatch[1]) ? videoIdMatch[1] : null;
+    return videoIdMatch && videoIdMatch[1] ? videoIdMatch[1] : null;
 }
 
-
-
 async function run() {
-    document.getElementById('data').innerHTML = '';
+    document.getElementById("data").innerHTML = "";
 
-    var url = document.getElementById('url').value;
+    var url = document.getElementById("url").value;
     const linksArr = url.split(" ");
     for (let i = 0; i < linksArr.length; i++) {
         var videoId = getYoutubeVideoId(linksArr[i]);
@@ -28,96 +27,51 @@ async function run() {
             return;
         }
         let response = await data(videoId, i);
-        res.push(response)
+        res.push(response);
     }
 
-    document.getElementById('url').value = '';
-    if (localStorage.getItem('apikey')) {
-        const loader = document.getElementById('loader');
-        loader.style.display = 'block';
+    document.getElementById("url").value = "";
+    if (localStorage.getItem("apikey")) {
+        const loader = document.getElementById("loader");
+        loader.style.display = "block";
 
         window.aiResponse();
-    }
-    else {
+    } else {
         alert("Please enter the API key first to use ai!!!");
         return;
     }
-
 }
 
 async function data(videoId, i) {
     let videoData = {};
     var apiURL = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&type=video&key=AIzaSyDsARzcDc-OiolLz4qG2a-yPRHhfxDrHSc`;
     await fetch(apiURL)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             // Accessing the snippet part of the response
-            const dataContainer = document.getElementById('data');
+            const dataContainer = document.getElementById("data");
+            let output = document.createElement("div");
 
-            // output container
-            let output = document.createElement('div');
-            output.setAttribute('class', 'output');
-
-            // thumbnail
-            let img = document.createElement('img');
             let imgURL = data.items[0].snippet.thumbnails.maxres.url;
-            img.setAttribute('src', imgURL);
-            img.setAttribute('class', 'thumnail')
-            output.appendChild(img);
-
-            // title
-            let title = document.createElement('p');
             let tit = data.items[0].snippet.title;
-            title.innerText = tit;
-            output.appendChild(title)
-
-            // download thumbnail
-            let downloadThumbnailbtn = document.createElement('span');
-            downloadThumbnailbtn.className = 'downloadThumbnail';
-            downloadThumbnailbtn.textContent = 'Download';
-            downloadThumbnailbtn.setAttribute('onclick', `downloadThumbnail(${i})`);
-            output.appendChild(downloadThumbnailbtn);
-
-            // description
-            // let desc = document.createElement('p');
             let des = data.items[0].snippet.description;
-            // desc.innerText = des;
-            // output.appendChild(desc)
-
-            // // tags
-            // let tagcontainer = document.createElement('div');
-            // tagcontainer.className = 'tagContainer';
             let tg = data.items[0].snippet.tags;
-            // tg.forEach(element =>{
-            //     let tagspan = document.createElement('span');
-            //     tagspan.textContent = element;
-            //     tagcontainer.appendChild(tagspan);
-            // })
-            // output.appendChild(tagcontainer);
 
+            output.className = "output";
+            let outputData = `
+                                <img src="${data.items[0].snippet.thumbnails.maxres.url}" class="thumnail">
+                                <p>${data.items[0].snippet.title}</p>
+                                <span class="downloadThumbnail" onclick="downloadThumbnail(${i})">Download</span>
+                                <button onclick="showDesc(${i})">Show Description</button>
+                                <button onclick="showTags(${i})">Show Tags</button>
+                            `;
+            output.innerHTML = outputData;
+            dataContainer.appendChild(output);
             videoData = { imgURL, tit, des, tg };
-
-            // description button
-            let descbtn = document.createElement('button');
-            descbtn.setAttribute('onclick', `showDesc(${i})`);
-            descbtn.textContent = "Show Description";
-            output.appendChild(descbtn)
-
-            // tag button
-            let tagbtn = document.createElement('button');
-            tagbtn.setAttribute('onclick', `showTags(${i})`);
-            tagbtn.textContent = "Show Tags";
-            output.appendChild(tagbtn)
-
-
-            dataContainer.appendChild(output)
-
         })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+        .catch((error) => {
+            alert("Error fetching data:", error);
         });
 
     return videoData;
 }
-
-
